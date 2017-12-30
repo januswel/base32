729a51719d3b023b50c22da4aa6eb59a8836ddf1
janus_wel <janus.wel.3@gmail.com>
2017-12-30 22:00:32 +0900

    :hammer: divide into files

---
+++ test/encode.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { describe, it } from 'kocha'
+import assert from 'assert'
+const AssertionError = assert.AssertionError
+
+import encode from '../src/encode'
+
+describe('encode', () => {
+  it('throws when non hex string', () => {
+    assert.throws(() => encode('td'), AssertionError)
+    assert.throws(() => encode('4ac'), AssertionError)
+  })
+  it('encodes 10 digits hex string to 8 chracters Base32 string', () => {
+    assert('SA2FM6EQ' === encode('1234567890'))
+  })
+  it('encodes 12 digits hex string to 16 chracters Base32 string', () => {
+    assert('SA2FM6EQV======' === encode('1234567890ab'))
+  })
+  it('encodes 14 digits hex string to 16 chracters Base32 string', () => {
+    assert('SA2FM6EQADG====' === encode('1234567890abcd'))
+  })
+  it('encodes 16 digits hex string to 16 chracters Base32 string', () => {
+    assert('SA2FM6EQADG6===' === encode('1234567890abcdef'))
+  })
+  it('encodes 18 digits hex string to 16 chracters Base32 string', () => {
+    assert('SA2FM6EQADG66E=' === encode('1234567890abcdef12'))
+  })
+})
