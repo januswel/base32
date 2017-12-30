35c46063cddd93cbb4b97ddb77344a8e18754ce0
janus_wel <janus.wel.3@gmail.com>
2017-12-30 23:46:09 +0900

    :sparkles: implement

---
+++ src/decode.js
@@ -0,0 +1,58 @@
+// @flow
+
+import assert from 'assert'
+import { shiftLeft, shiftRight } from './bit-shift'
+
+const CHUNK_SIZE = 8
+const TABLE = (() => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'.split('')
+  const table = new Map()
+  characters.forEach((character, index) => {
+    table.set(character, index)
+  })
+  return table
+})()
+const PADDING_CHARACTER = '='
+
+const decodeChunk = (chunk: Base32String) => {
+  let numofPaddingCharacters = 0
+  const n = chunk.split('').reduce((sum, character, index) => {
+    if (character === PADDING_CHARACTER) {
+      ++numofPaddingCharacters
+      return sum
+    }
+    const current = TABLE.get(character)
+    sum += shiftLeft(current, (7 - index) * 5)
+    return sum
+  }, 0)
+
+  /**
+   * bit  | number of padding characters
+   * -----|-----------------------------
+   *  8   | 6
+   * 16   | 4
+   * 24   | 3
+   * 32   | 1
+   * */
+  const digits = Math.floor((40 - numofPaddingCharacters * 5) / 8)
+  return n.toString(16).slice(0, digits * 2)
+}
+
+const decode = (src: Base32String): HexString => {
+  assert(
+    src.length % 8 === 0 && /^[2-7A-Z]+=*$/.test(src),
+    'src must be a Base32 string',
+  )
+
+  const numofChunks = src.length / CHUNK_SIZE
+
+  const decoded = []
+  for (let i = 0; i < numofChunks; ++i) {
+    const start = i * CHUNK_SIZE
+    const chunk = src.slice(start, start + CHUNK_SIZE)
+
+    decoded.push(decodeChunk(chunk))
+  }
+  return decoded.join('')
+}
+export default decode
--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 // @flow
 
 export { default as encode } from './encode'
-
-const decode = (src: Base32String): HexString => '1234567890abcdef'
-export { decode }
+export { default as decode } from './decode'
