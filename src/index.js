6870ab9208d7f97bfc002ab3fa7f2c1e6c5c148d
janus_wel <janus.wel.3@gmail.com>
2017-12-30 20:37:30 +0900

    :hammer: refactor

--- src/index.js
+++ src/index.js
@@ -12,11 +12,13 @@ const mask = (n, digit) => (n & (MASK << digit)) >>> digit
  * */
 const NUMOF_PROCESSED_CHARACTERS = 10
 const TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+const PADDING_CHARACTER = '='
 
 const encode = (src: HexString): Base32String => {
-  const NUMOF_PROCESSES =
-    Math.floor(src.length / NUMOF_PROCESSED_CHARACTERS) + 1
-  const NUMOF_CHARACTERS_LAST = src.length % NUMOF_PROCESSED_CHARACTERS
+  const NUMOF_PROCESSES = Math.ceil(src.length / NUMOF_PROCESSED_CHARACTERS)
+  const FRACTIONS = src.length % NUMOF_PROCESSED_CHARACTERS
+  const NUMOF_CHARACTERS_LAST =
+    FRACTIONS === 0 ? NUMOF_PROCESSED_CHARACTERS : FRACTIONS
 
   const encoded = []
   for (let i = 0; i < NUMOF_PROCESSES; ++i) {
@@ -28,59 +30,20 @@ const encode = (src: HexString): Base32String => {
         : start + NUMOF_PROCESSED_CHARACTERS,
     )
 
-    switch (chunk.length) {
-      case 10: {
-        const n = parseInt(chunk, 16) << 0
-        for (let j = 35; 0 <= j; j -= 5) {
-          encoded.push(TABLE[mask(n, j)])
-        }
-        break
-      }
-      case 8: {
-        const n = parseInt(chunk, 16) << 8
-        for (let j = 35; 6 <= j; j -= 5) {
-          encoded.push(TABLE[mask(n, j)])
-        }
-        for (let j = 0; j < 1; ++j) {
-          encoded.push('=')
-        }
-        break
-      }
-      case 6: {
-        const n = parseInt(chunk, 16) << 16
-        for (let j = 35; 16 <= j; j -= 5) {
-          encoded.push(TABLE[mask(n, j)])
-        }
-        for (let j = 0; j < 3; ++j) {
-          encoded.push('=')
-        }
-        break
-      }
-      case 4: {
-        const n = parseInt(chunk, 16) << 24
-        for (let j = 35; 21 <= j; j -= 5) {
-          encoded.push(TABLE[mask(n, j)])
-        }
-        for (let j = 0; j < 4; ++j) {
-          encoded.push('=')
-        }
-        break
-      }
-      case 2: {
-        const n = parseInt(chunk, 16) << 32
-        for (let j = 35; 31 <= j; j -= 5) {
-          encoded.push(TABLE[mask(n, j)])
-        }
-        for (let j = 0; j < 6; ++j) {
-          encoded.push('=')
-        }
-        break
-      }
+    const shiftWidth = (10 - chunk.length) * 4
+    const numofPadded = Math.floor(shiftWidth / 5)
+    const n = parseInt(chunk, 16) << shiftWidth
+    for (let j = 7; numofPadded < j; --j) {
+      encoded.push(TABLE[mask(n, j * 5)])
+    }
+    if (numofPadded === 0) {
+      encoded.push(TABLE[mask(n, 0)])
+    }
+    for (let j = 0; j < numofPadded; ++j) {
+      encoded.push(PADDING_CHARACTER)
     }
   }
   return encoded.join('')
-
-  return 'GEZDGNBVGY3TQOJQMFRGGZDFMY======'
 }
 const decode = (src: Base32String): HexString => '1234567890abcdef'
 
